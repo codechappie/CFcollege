b441005a917fffdc2c0a80b65ef4399759dd46d5
codechappie <davidproyecto01@gmail.com>
2018-07-28 15:46:51 -0500

    Subida de CFcollege

---
+++ pages/extras/ini.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Lockscreen</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
+<link rel="stylesheet" href="esti.css">
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition lockscreen">
+<!-- Automatic element centering -->
+<div class="lockscreen-wrapper">
+  <div class="lockscreen-logo">
+    <a href="../../index2.php"><b>CF</b>college</a>
+    <h5>Sistema de seguimiento escolar</h5>
+  </div>
+
+
+
+
+
+
+  <div class="lockscreen-footer text-center">
+    <div class="box-body">
+
+                  <a class="btn btn-app">
+                    <i class="fa fa-edit"></i> Edit
+                  </a>
+                  <a class="btn btn-app">
+                    <i class="fa fa-play"></i> Play
+                  </a>
+                  <a class="btn btn-app">
+                    <i class="fa fa-repeat"></i> Repeat
+                  </a>
+                  <a class="btn btn-app">
+                    <i class="fa fa-pause"></i> Pause
+                  </a>
+                  <a class="btn btn-app">
+                    <i class="fa fa-edit"></i> Edit
+                  </a>
+                  <a class="btn btn-app">
+                    <i class="fa fa-save"></i> Save
+                  </a>
+                  <a class="btn btn-app">
+                    <span class="badge bg-yellow">3</span>
+                    <i class="fa fa-bullhorn"></i> Notifications
+                  </a>
+                  <a class="btn btn-app">
+                    <span class="badge bg-green">300</span>
+                    <i class="fa fa-barcode"></i> Products
+                  </a>
+                  <a class="btn btn-app">
+                    <span class="badge bg-purple">891</span>
+                    <i class="fa fa-users"></i> Users
+                  </a>
+                  <a class="btn btn-app">
+                    <span class="badge bg-teal">67</span>
+                    <i class="fa fa-inbox"></i> Orders
+                  </a>
+                  <a class="btn btn-app">
+                    <span class="badge bg-aqua">12</span>
+                    <i class="fa fa-envelope"></i> Inbox
+                  </a>
+                  <a class="btn btn-app">
+                    <span class="badge bg-red">531</span>
+                    <i class="fa fa-heart-o"></i> Likes
+                  </a>
+<br>
+<br>
+    Copyright &copy; 2014-2016 <b><a href="https://adminlte.io" class="text-black">Almsaeed Studio</a></b><br>
+    All rights reserved
+  </div>
+</div>
+</div>
+<!-- /.center -->
+
+<!-- jQuery 3 -->
+<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ principal01.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Principal</title>
+    <link rel="stylesheet" href="boo/css/bootstrap.min.css">
+    <link rel="stylesheet" href="esti.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Assistant:200" rel="stylesheet">
+  </head>
+  <body style="background: radial-gradient(rgb(93, 143, 250), rgb(76, 108, 214), rgb(41, 85, 179));">
+    <div class="container">
+
+  <div class="row justify-content-center">
+    <div class="imog col-2">
+      <div class="login-logo">
+        <h2 style="color: white" href="#"><b>CF</b>college</h2>
+      </div>
+    </div>
+  </div>
+<br><br>
+
+<div class="card-group">
+  <div onclick="location.href='#';" class="card bg-1">
+    <i class="fa fa-user"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a  class="card-title">Información Personal</a>
+    </div>
+  </div>
+  <div class="card bg-2">
+    <i class="fa fa-calendar-alt"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Asistencias</a>
+    </div>
+  </div>
+  <div class="card bg-3">
+    <i class="fa fa-file-alt"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Calificaciones</a>
+    </div>
+  </div>
+  <div class="card bg-4">
+    <i class="far fa-comments"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Comunicados</a>
+    </div>
+  </div>
+  <div class="card bg-5">
+    <i class="fa fa-clipboard"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Encuestas</a>
+    </div>
+  </div>
+
+</div>
+
+<div class="card-group">
+  <div class="card bg-1a">
+    <i class="fa fa-user"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Reportes</a>
+    </div>
+  </div>
+  <div class="card bg-2a">
+    <i class="fa fa-calendar-alt"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Mensajes</a>
+    </div>
+  </div>
+  <div class="card bg-3a">
+    <i class="fa fa-file-alt"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Horarios</a>
+    </div>
+  </div>
+  <div class="card bg-4a">
+    <i class="far fa-comments"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Recursos</a>
+    </div>
+  </div>
+  <div class="card bg-5a">
+    <i class="fa fa-clipboard"></i>
+    <!--<img class="card-img-top" src="dist/img/avatar.png" alt="Card image cap">-->
+    <div class="card-body">
+      <a class="card-title">Historial Económico</a>
+    </div>
+  </div>
+
+</div>
+
+
+
+  <script src="boo/js/jquery.js" charset="utf-8"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="boo/js/bootstrap.min.js" charset="utf-8"></script>
+  </body>
+</html>
